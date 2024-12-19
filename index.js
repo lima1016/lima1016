6a6355d8335259a89653c350bfce12cd8bc99c2f
YoulimKim <kimyoulim1016@gmail.com>
2024-12-19 20:59:00 +0900

    .

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ let text = `
 <img src="https://img.shields.io/badge/IntelliJIDEA-000000?style=flat-square&logo=IntelliJIDEA&logoColor=white"/>
 <img src="https://img.shields.io/badge/Jira-0052CC?style=flat-square&logo=Jira&logoColor=white"/>
 
-</b> My Tistory... 😎 </b>
+## 😎Lastest Blog Posts
 </div>
 
 `;
