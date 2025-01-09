5a5c4230e7fc4a376f6cd813afe78120ca4533ce
YoulimKim <kimyoulim1016@gmail.com>
2025-01-10 00:41:10 +0900

    d

--- index.js
+++ index.js
@@ -7,33 +7,38 @@ import Parser from "rss-parser";
  */
 let text = `
 <div align="center">
+  
+![header](https://capsule-render.vercel.app/api?type=venom&color=auto&height=300&section=header&text=Hello%Lima!&fontSize=90)
+<a href="https://github.com/devxb/gitanimals">
+<img src="https://render.gitanimals.org/farms/lima1016"/>
+</a>
+[![trophy](https://github-profile-trophy.vercel.app/?username=lima1016&row=1)](https://github.com/lima1016/github-profile-trophy)
 
-  ![header](https://capsule-render.vercel.app/api?type=cylinder&color=auto&height=100&section=header&text=Hello%Lima!&fontSize=70)
-  [![trophy](https://github-profile-trophy.vercel.app/?username=lima1016&row=1)](https://github.com/lima1016/github-profile-trophy)
-  ![lima1016's github stats](https://github-readme-stats.vercel.app/api?username=lima1016&show_icons=true&theme=synthwave&hide_border=true)
-  <a href="https://github.com/devxb/gitanimals">
-  <img src="https://render.gitanimals.org/farms/lima1016"/>
-  </a>
-
-  <br>
-
-</b> 🌱 I’m currently learning! </b> <br><br>
+<br>   
+  
+</b> [![Typing SVG](https://readme-typing-svg.demolab.com/?lines=🌱+I’m+currently+learning!)](https://git.io/typing-svg) </b> <br>
 <img src="https://img.shields.io/badge/Java-FF0000?style=flat-square&logo=Java&logoColor=white"/>
+<img src="https://img.shields.io/badge/python-1313CB?style=flat-square&logo=Python&logoColor=white"/>
 <img src="https://img.shields.io/badge/Apachekafka-231F20?style=flat-square&logo=apachekafka&logoColor=white"/>
-<img src="https://img.shields.io/badge/Spring-6DB33F?style=flat-square&logo=Spring&logoColor=white"/>
 <img src="https://img.shields.io/badge/Redis-DC382D?style=flat-square&logo=Redis&logoColor=white"/>
-<img src="https://img.shields.io/badge/Gradle-02303A?style=flat-square&logo=Gradle&logoColor=white"/>
+<img src="https://img.shields.io/badge/Spring-6DB33F?style=flat-square&logo=Spring&logoColor=white"/>
+<img src="https://img.shields.io/badge/SpringBoot-6DB33F?style=flat-square&logo=SpringBoot&logoColor=white"/>
+<img src="https://img.shields.io/badge/SpringCloud-6DB33F?style=flat-square&logo=springcloud&logoColor=white"/>
 <img src="https://img.shields.io/badge/PostgreSQL-4169E1?style=flat-square&logo=PostgreSQL&logoColor=white"/>
 <img src="https://img.shields.io/badge/MariaDB-003545?style=flat-square&logo=MariaDB&logoColor=white"/>
 <img src="https://img.shields.io/badge/MongoDB-47A248?style=flat-square&logo=MongoDB&logoColor=white"/>
-<img src="https://img.shields.io/badge/Postman-FF6C37?style=flat-square&logo=Postman&logoColor=white"/>
-<img src="https://img.shields.io/badge/OAuth-EB5424?style=flat-square&logo=Auth0&logoColor=white"/> <br>
-<img src="https://img.shields.io/badge/Json-000000?style=flat-square&logo=Json&logoColor=white"/>
-<img src="https://img.shields.io/badge/SpringBoot-6DB33F?style=flat-square&logo=SpringBoot&logoColor=white"/>
+<img src="https://img.shields.io/badge/OpenStack-C31B1B?style=flat-square&logo=OpenStack&logoColor=white"/>
 <img src="https://img.shields.io/badge/Apache-D22128?style=flat-square&logo=Apache&logoColor=white"/>
 <img src="https://img.shields.io/badge/ApacheTomcat-F8DC75?style=flat-square&logo=ApacheTomcat&logoColor=white"/>
 <img src="https://img.shields.io/badge/ApacheMaven-C71A36?style=flat-square&logo=ApacheMaven&logoColor=white"/>
+<img src="https://img.shields.io/badge/Gradle-02303A?style=flat-square&logo=Gradle&logoColor=white"/>
+<img src="https://img.shields.io/badge/Docker-1F4BC6?style=flat-square&logo=Docker&logoColor=white"/>
+<img src="https://img.shields.io/badge/Jenkins-E5145A?style=flat-square&logo=Jenkins&logoColor=white"/>
+<img src="https://img.shields.io/badge/Nginx-21D262?style=flat-square&logo=Nginx&logoColor=white"/>
+<img src="https://img.shields.io/badge/OAuth-EB5424?style=flat-square&logo=Auth0&logoColor=white"/>
+<img src="https://img.shields.io/badge/JWT-58C5F3?style=flat-square&logo=jwt&logoColor=white"/>
 <img src="https://img.shields.io/badge/IntelliJIDEA-000000?style=flat-square&logo=IntelliJIDEA&logoColor=white"/>
+<img src="https://img.shields.io/badge/Postman-FF6C37?style=flat-square&logo=Postman&logoColor=white"/>
 <img src="https://img.shields.io/badge/Jira-0052CC?style=flat-square&logo=Jira&logoColor=white"/>
 
 ## 😎Lastest Blog Posts
